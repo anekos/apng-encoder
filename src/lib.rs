a9fd3c32ba64139f1e26742392b1f6c26b80bce7
anekos <anekos@snca.net>
2019-01-19 21:00:54 +0900

    Add a test `generate_4frames`

--- src/lib.rs
+++ src/lib.rs
@@ -7,10 +7,10 @@ pub mod apng;
 #[cfg(test)]
 mod tests {
     #[test]
-    fn it_works() {
+    fn generate_simple_2x2() {
+        use std::fs::{create_dir, File};
         use crate::apng::{Color, Delay, Frame, Meta};
         use crate::apng::encoder::{Encoder, Filter};
-        use std::fs::File;
 
         // Generate 2x2 Animated PNG (4 frames)
         let meta = Meta {
@@ -33,7 +33,8 @@ mod tests {
             ..Default::default()
         };
 
-        let mut file = File::create("test.png").unwrap();
+        let mut file = File::create("test-output/2x2.png").unwrap();
+        let _ = create_dir("test-output");
         let mut encoder = Encoder::create(&mut file, &meta).unwrap();
 
         // RED   GREEN
@@ -79,6 +80,50 @@ mod tests {
             Some(&frame),
             filter).unwrap();
         // !!IMPORTANT DONT FORGET!!
+        encoder.finish().unwrap();
+    }
+
+    #[test]
+    fn generate_4frames() {
+        use std::fs::{create_dir, File};
+        use image::png::PNGDecoder;
+        use image::ImageDecoder;
+        use crate::apng::{Color, Delay, Frame, Meta};
+        use crate::apng::encoder::{Encoder, Filter};
+
+
+        // Generate 2x2 Animated PNG (4 frames)
+        let meta = Meta {
+            width: 716,
+            height: 660,
+            color: Color {
+                alpha_channel: false,
+                bit_depth: 8,
+                grayscale: false,
+            },
+            frames: 4,
+            plays: None, // Infinite loop
+        };
+
+        let filter = Some(Filter::Up);
+
+        // Delay = 2 seconds
+        let frame = Frame {
+            delay: Some(Delay::new(1, 10)),
+            ..Default::default()
+        };
+
+        let mut file = File::create("test-output/cherenkov.png").unwrap();
+        let mut encoder = Encoder::create(&mut file, &meta).unwrap();
+
+        for i in 1 ..= 4 {
+            let source_file = File::open(format!("test-files/{}.png", i)).unwrap();
+            let _ = create_dir("test-output");
+            let decoder = PNGDecoder::new(source_file).unwrap();
+            let image = decoder.read_image().unwrap();
+            encoder.write_frame(&image, None, Some(&frame), filter).unwrap();
+        }
+
         encoder.finish().unwrap();
     }
 }
